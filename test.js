4beb892e1ceef5aadecbdd04879ca76c3514b500
ni_n_ja <rampage4extrm@gmail.com>
2017-02-04 01:48:58 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+'use strict'
+"た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎す͜͜͏̘̣͔͙͎͎ơ̟̤̖̗͖͇̍͋̀͆̓́͞͡け̜ͪ̅̍̅͂͊て".split("").forEach(function(e, index, array) {
+    console.log(e.codePointAt());
+    console.log(e.charCodeAt(0));
+    console.log(String.fromCodePoint(e.charCodeAt(0)));
+});
